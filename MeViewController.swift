b924042084df181e78f7796807f216c026175f4b
Dmitriy Loria <dima_loria@mail.ru>
2019-10-16 22:57:03 +0300

    AboutMe

---
+++ MeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MeViewController.swift
+//  BullsEye
+//
+//  Created by Dima Loria on 06/10/2019.
+//  Copyright © 2019 Dima Loria. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+        }
+    @IBAction func back (){
+    dismiss(animated: true, completion: nil)
+    }
+    
+}
